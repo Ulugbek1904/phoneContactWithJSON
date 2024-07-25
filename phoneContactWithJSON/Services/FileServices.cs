6f1780ff898fdf471757a65fc5c9fb1d771731f3
Ulugbek1904 <julugbek023@gmail.com>
2024-07-25 22:42:39 +0500

    Add Function

---
+++ phoneContactWithJSON/Services/FileServices.cs
@@ -0,0 +1,70 @@
+﻿using phoneContactWithJSON.Models;
+using phoneContactWithJSON.Services;
+
+namespace PhoneContacts.Services
+{
+    internal class FileServices : IFileServices
+    {
+        private const string filePath = 
+            "../../../phoneContacts.txt";
+
+        private ILoggingServices loggingServices;
+        public FileServices()
+        {
+            this.loggingServices =
+                new LoggingServices();
+
+            EnsureFileExist();
+        }
+
+        private static void EnsureFileExist()
+        {
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+        }
+
+        public List<PhoneContact> Readlines()
+        {
+            List<PhoneContact> phoneContacts = new List<PhoneContact>();
+
+            string[] allLines = File.ReadAllLines(filePath);
+
+            foreach (string line in allLines)
+            {
+                var contact = new PhoneContact();
+                var words = line.Split(' ');
+
+                if (words.Length > 2 && words[0] == "Name:")
+                {
+                    // Extracting the name and phone number correctly
+                    contact.Name = words[1];
+                    contact.PhoneNumber = words[3];
+                }
+
+                phoneContacts.Add(contact);
+            }
+
+            return phoneContacts;
+        }
+
+        public void AddContact(PhoneContact contact)
+        {
+            var contactLine = $"Name: {contact.Name}; " +
+                $"Number: {contact.PhoneNumber}";
+
+            File.AppendAllText(filePath, 
+                contactLine + Environment.NewLine);
+        }
+
+        public void SaveAllContacts(List<PhoneContact> contacts)
+        {
+            var lines = contacts.Select(contact => 
+            $"Name: {contact.Name} Number: " +
+            $"{contact.PhoneNumber}");
+
+            File.WriteAllLines(filePath, lines);
+        }
+    }
+}
